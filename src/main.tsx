795f036ce596ab6873c744dc950e0ba6d770c82d
Sachin Parshetti <sachin.parshetti123@gmail.com>
2025-07-27 20:59:44 +0530

    tsconfig file updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Auth0Provider } from "@auth0/auth0-react"
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { store } from './Redux/Store.ts'
+import { store } from  './Redux/Store'
 
 
 
